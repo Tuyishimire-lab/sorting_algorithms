41d3a1ac905ea7cbfe9163816f3779f7223299c6
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-10-25 19:31:31 +0200

    spacing fixation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,28 +5,29 @@
  * @size: size of array
  * 
  */
-void selection_sort(int *array, size_t size){
-    int index = 0, min, tmp;
-    size_t i, j;
-    if( array == NULL || size < 2)
-        return;
-    for (i =0; i< size -1; i++)
-    {
-        min = array[i];
-        for(j = i + 1; j< size; j++)
+void selection_sort(int *array, size_t size)
+{
+        int index = 0, min, tmp;
+        size_t i, j;
+        if( array == NULL || size < 2)
+                return;
+        for (i =0; i< size -1; i++)
         {
-            if(array[j] < min)
-            {
-                min = array[j];
-                index = j;
-            }
+                min = array[i];
+                for(j = i + 1; j< size; j++)
+                {
+                        if(array[j] < min)
+                        {
+                                min = array[j];
+                                index = j;
+                        }
+                }
+                if(min != array[i])
+                {
+                        tmp = array[i];
+                        array[i] = array[index];
+                        array[index] = tmp;
+                        print_array(array, size);
+                }
         }
-        if(min != array[i])
-        {
-            tmp = array[i];
-            array[i] = array[index];
-            array[index] = tmp;
-            print_array(array, size);
-        }
-    }
 }

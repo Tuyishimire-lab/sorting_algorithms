b50bf21cd69c1006033521ab311814f52f2a1efc
Tuyishimire.lab <tuyishime1angel@gmail.com>
2021-10-25 14:21:20 -0400

    bubble sorting function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,36 @@
 #include "sort.h"
 /**
  * bubble_sort - sorts bubbly
- *  @array: array to sort
+ * @array: array to sort
  * @size: size
- * 
+ *
  */
 
-void bubble_sort(int * array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
-        size_t i, newsize = size;
-        int tmp, swap;
+	size_t i, newsize = size;
+	int tmp,swap;
 
-        if (array == NULL || size < 2)
-            return;
+	if (array == NULL || size < 2)
+		return;
 
-        while(newsize)
-        {
-                swap = 0;
-                for (i = 0; i < newsize - 1; i++)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                tmp = array[i + 1];
-                                array[i + 1] = array[i];
-                                array[i] = tmp;
-                                print_array(array, size);
-                                swap = 1;
-                        }
-                }
-                newsize--;
-                if (!swap)
-                    break;
-        }
+	while (newsize)
+	{
+		swap = 0;
+		for (i = 0; i < newsize - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		newsize--;
+		if (!swap)
+			break;
+	}
 }
+

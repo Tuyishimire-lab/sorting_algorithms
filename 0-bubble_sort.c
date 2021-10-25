4b344506adfcf7e6a294f54572fa8aa6e334e317
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-10-25 19:16:40 +0200

    spacing correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,29 +6,30 @@
  * 
  */
 
-void bubble_sort(int * array, size_t size){
-    size_t i, newsize = size;
-    int tmp, swap;
+void bubble_sort(int * array, size_t size)
+{
+        size_t i, newsize = size;
+        int tmp, swap;
 
-    if (array == NULL || size < 2)
-    return;
+        if (array == NULL || size < 2)
+            return;
 
-    while(newsize)
-    {
-        swap = 0;
-        for (i = 0; i < newsize - 1; i++)
+        while(newsize)
         {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = tmp;
-                print_array(array, size);
-                swap = 1;
-            }
+                swap = 0;
+                for (i = 0; i < newsize - 1; i++)
+                {
+                        if (array[i] > array[i + 1])
+                        {
+                                tmp = array[i];
+                                array[i] = array[i+1];
+                                array[i+1] = tmp;
+                                print_array(array, size);
+                                swap = 1;
+                        }
+                }
+                newsize--;
+                if (!swap)
+                    break;
         }
-        newsize--;
-        if (!swap)
-            break;
-    }
 }
